faa739f40600b0e887933e99edf738aff77774e8
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-05-22 02:04:43 +0900

    6:15

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,21 +27,24 @@
         </div>
     </div>
 
-    <div class="flex flex-col bg-pink-500 w-full h-screen">
-        <p class="text-white text-5xl font-extrabold text-center">
-            Lets do it!
-        </p>
-        <form action="">
-            <x-input
-            class="px-5 py-3 w-80 border border-blue-400"
-            type="email"
-            name="email"
-            placeholder="Email address"
-            ></x-input>
-            <span class="text-gray-100 text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate accusamus, quaerat, incidunt suscipit omnis deleniti aut, voluptate molestias natus quibusdam nisi assumenda aliquid. Minima nesciunt corporis quos autem laudantium explicabo!</span>
-            <x-button class="px-5 py-8 mt-5 w-80 bg-blue-500 justify-center">
-                Get In 
-            </x-button>
-        </form>
+
+    <div class="flex fixed top-0 bg-gray-900 bg-opacity-60 items-center w-full h-full">
+        <div class="bg-pink-500 m-auto shadow-2xl rounded-xl p-8">
+            <p class="text-white text-5xl font-extrabold text-center">
+                Lets do it!
+            </p>
+            <form action="" class="flex flex-col items-center p-24">
+                <x-input
+                class="px-5 py-3 w-80 border border-blue-400"
+                type="email"
+                name="email"
+                placeholder="Email address"
+                ></x-input>
+                <span class="text-gray-100 text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+                <x-button class="px-5 py-8 mt-5 w-80 bg-blue-500 justify-center">
+                    Get In 
+                </x-button>
+            </form>
+        </div>
     </div>
 </x-guest-layout>
