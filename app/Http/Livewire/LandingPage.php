cd16722a57745666c71aa9e4033fda5aa484056d
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-07-09 16:02:16 +0900

    16

--- app/Http/Livewire/LandingPage.php
+++ app/Http/Livewire/LandingPage.php
@@ -2,16 +2,22 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Subscriber;
 use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 
 class LandingPage extends Component
 {
-    public $email;
+    public $email = '';
 
     public function subscribe()
     {
-        Log::debug($this->email);
+        // Log::debug($this->email);
+        $subscriber = Subscriber::create([
+            'email' => $this->email,
+        ]);
+
+        $this->reset('email');
     }
 
     public function render()
